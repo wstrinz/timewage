d51e077c4a61f1cf48e4928ff66b35adc89e86c9
Will Strinz <wstrinz@gmail.com>
2018-12-10 08:41:57 -0600

    version bumps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TimeWage.MixProject do
     [
       app: :time_wage,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       default_task: "get_balance",
@@ -32,7 +32,7 @@ defmodule TimeWage.MixProject do
       {:poison, "~> 3.1"},
       {:csv, "~> 2.0.0"},
       {:timex, "~> 3.1"},
-      {:httpoison, "~> 1.0"},
+      {:httpoison, "~> 1.4"},
       {:exvcr, "~> 0.10", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
